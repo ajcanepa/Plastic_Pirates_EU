72281199272104867032d86a34b1d693489ff1aa
ajcanepa <ajcanepa@ubu.es>
2025-07-29 10:08:29 +0200

    Limpieza resultados, no funciona

--- Save_Render_Results.R
+++ Save_Render_Results.R
@@ -4,17 +4,27 @@
 #   output_format = "html_notebook",
 #   output_file = "Plastic_Pirates_Paper_I_Results.nb.html",
 #   output_dir = "OUTPUT/RESULTS",
+#   intermediates_dir = "OUTPUT/RESULTS",
 #   clean = FALSE
 # )
 
 
 # Render .html and .md files ----------------------------------------------
-rmarkdown::render(
-  "Plastic_Pirates_EU_Analysis.Rmd",
-  output_format = "html_document",
-  output_file = "OUTPUT/RESULTS/Plastic_Pirates_Paper_I_Results.html",
-  clean = FALSE
-)
+# rmarkdown::render(
+#   "Plastic_Pirates_EU_Analysis.Rmd",
+#   output_format = "html_document",
+#   output_file = "OUTPUT/RESULTS/Plastic_Pirates_Paper_I_Results.html",
+#   clean = FALSE
+# )
+# 
+# rmarkdown::render(
+#   "Plastic_Pirates_EU_Analysis.Rmd",
+#   output_format = "html_document",
+#   output_file = "Plastic_Pirates_Paper_I_Results.html",
+#   output_dir = "OUTPUT/RESULTS",
+#   intermediates_dir = "OUTPUT/RESULTS",
+#   clean = FALSE
+# )
 
 # # Render a .md file -------------------------------------------------------
 # knitr::knit("Plastic_Pirates_EU_Analysis.Rmd", output = "OUTPUT/RESULTS/Plastic_Pirates_Paper_I_Results.md")
